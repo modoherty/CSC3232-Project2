6b364d99e61e9476be60f382f2ce835d5a5496cc
modoherty <b9015456@newcastle.ac.uk>
2021-12-09 21:33:37 +0000

    Added descriptive comments to AudioManagement class

--- Assets/Scripts/AudioManagement.cs
+++ Assets/Scripts/AudioManagement.cs
@@ -4,19 +4,25 @@ using UnityEngine.Audio;
 
 public class AudioManagement : MonoBehaviour
 {
+    // Array of sound clips
     public Audio[] audios;
 
     void Awake()
     {
+        // Adds an audio source for each specific sound to the AudioManagement class
         foreach(Audio a in audios)
         {
             a.audioSource = gameObject.AddComponent<AudioSource>();
             a.audioSource.clip = a.audioClip;
 
+            // Attributes for the sound - volume, pitch and whether it should loop
             a.audioSource.volume = a.volume;
             a.audioSource.pitch = a.pitch;
             a.audioSource.loop = a.loop;
 
+            /* Ensures that no sounds play immediately, to prevent them from all
+               playing at the same time when the audio manager is reloaded (e.g. 
+               resuming the game from the pause menu) */
             a.audioSource.playOnAwake = false;
         }
 
@@ -24,8 +30,10 @@ public class AudioManagement : MonoBehaviour
 
     public void Start()
     {
+        // Gets the currently active scene - the level the player is on
         string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
 
+        // Plays each level's specific theme
         if (currentScene == "MainMenu")
             Play("MainMenu");
 
@@ -39,15 +47,21 @@ public class AudioManagement : MonoBehaviour
             Play("Level2");
     }
 
+    // This method takes the name of the audio clip and will play it in the level.
     public void Play(string audioName)
     {
+        // Finds the correct sound in the audios array
         Audio a = Array.Find(audios, sound => sound.audioName == audioName);
+        // Plays the audio
         a.audioSource.Play();
     }
 
+    // This method will pause the given audio.
     public void Pause(string audioName)
     {
+        // Finds the correct sound in the audios array
         Audio a = Array.Find(audios, sound => sound.audioName == audioName);
+        // Pauses the audio
         a.audioSource.Pause();
     }
 }
